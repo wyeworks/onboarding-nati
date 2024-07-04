9e53c84186ca3a71313c1c0ff232b7fac5de9ecc
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-04 11:57:30 -0300

    CrewMember, Passenger, Pilot, Steward

---
+++ app/models/crew_member.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  nationality :string
+#  passaport   :string
+#  phone       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class CrewMember < Person
+end
