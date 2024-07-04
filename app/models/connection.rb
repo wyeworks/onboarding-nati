c162abf108caa0abfdaca7ad1a5caaabe468dbcb
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2024-07-04 11:57:08 -0300

    Oh no, no puedo editar la schema information

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,7 +3,7 @@
 # Table name: trips
 #
 #  id            :integer          not null, primary key
-# TODO: verificar si es una buena idea borrar la linea del flight number en lugar de ponerlo null
+#  flight_number :integer
 #  type          :string
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,7 @@
 # Table name: trips
 #
 #  id            :integer          not null, primary key
-#  flight_number :integer          null
+#  flight_number :integer
 #  type          :string
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
